e91c9c9c2fec9f8d7b1687447c26cba263379acb
Thorsten Hans <thorsten.hans@gmail.com>
2021-04-21 18:44:19 +0000

    chore: tidy up meta.tf

--- meta.tf
+++ meta.tf
@@ -7,11 +7,11 @@ terraform {
   required_version = "=0.15.0"
 
   backend "azurerm" {
-    resource_group_name = "rg-escp-state-backend"
+    resource_group_name  = "rg-escp-state-backend"
     storage_account_name = "saespc2021state"
-    container_name      = "terraform-state"
-    key                 = "espc2021.tfstate"
-    use_azuread_auth    = true
+    container_name       = "terraform-state"
+    key                  = "espc2021.tfstate"
+    use_azuread_auth     = true
   }
 
   required_providers {
