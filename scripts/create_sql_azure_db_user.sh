1705aad4e9d684ee5fd40d406a6580ee1ac80f80
Thorsten Hans <thorsten.hans@gmail.com>
2021-04-21 22:17:40 +0000

    chore: do not delete firewall rule

--- scripts/create_sql_azure_db_user.sh
+++ scripts/create_sql_azure_db_user.sh
@@ -6,9 +6,9 @@ echo 'Creating User with Role Assignments for Datbase: ' $SQL_AZURE_CONTENT_DB_N
 
 sqlcmd -S $SQL_AZURE_FQDN -U $SQL_AZURE_ADMIN_USER -P $SQL_AZURE_ADMIN_PASSWORD -d $SQL_DB_NAME -i ./scripts/create_sqlazure_user.sql -v SqlUserName=$SQL_AZURE_USER_NAME
 
-az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET -t $ARM_TENANT_ID
-az account set --subscription $ARM_SUBSCRIPTION_ID
+# az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET -t $ARM_TENANT_ID
+# az account set --subscription $ARM_SUBSCRIPTION_ID
 
 # echo 'Removing SQL Azure Firewall Rule'
-az sql server firewall-rule delete -n $SQL_AZURE_FIREWALL_RULE_NAME -g $RESOURCE_GROUP_NAME -s $SQL_AZURE_SERVER_NAME
-az logout --username $ARM_CLIENT_ID
+# az sql server firewall-rule delete -n $SQL_AZURE_FIREWALL_RULE_NAME -g $RESOURCE_GROUP_NAME -s $SQL_AZURE_SERVER_NAME
+# az logout --username $ARM_CLIENT_ID
